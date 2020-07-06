65fb2268cf43d1148ef5e84fafd3e061c03e3cb0
Leo <hoangtt995@gmail.com>
2020-07-05 22:08:03 -0400

    - add middleware auth to ask user to sign in to post - use auth()->user()->posts() to create posts so it will create post inside user... - to truncate in tinker: e.g. Post::truncate(); it will erase everything in Post.. - composer require intervention/image for laravel

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PostsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
     {
         return view('posts.create');
@@ -19,9 +26,21 @@ class PostsController extends Controller
            'image' => ['required', 'image'],
         ]);
 
-        Post::create($data);
+        $imagePath = request('image')->store('uploads', 'public');
 
-        dd(request()->all());
+        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+        $image->save();
+
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath,
+        ]);
+
+        return redirect('/profile/' . auth()->user()->id);
     }
 
+    public function show($post)
+    {
+        dd($post);
+    }
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,10 +9,10 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="#">Add New Post</a>
+                <a href="/p/create">Add New Post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>100</strong> posts</div>
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>100k</strong> followers</div>
                 <div class="pr-5"><strong>100k</strong> following</div>
             </div>
@@ -23,15 +23,13 @@
     </div>
 
     <div class="row pt-5">
-        <div class="col-4">
-            <img src="/images/laravel7.jpg" class="w-100" />
-        </div>
-        <div class="col-4">
-            <img src="/images/laravel7.jpg" class="w-100" />
-        </div>
-        <div class="col-4">
-            <img src="/images/laravel7.jpg" class="w-100" />
-        </div>
+        @foreach($user->posts as $post)
+            <div class="col-4 pb-4">
+                <a href="/p/{{ $posts->id }}">
+                    <img src="/storage/{{ $post->image }}" class="w-100" />
+                </a>
+            </div>
+        @endforeach
     </div>
 </div>
 @endsection
